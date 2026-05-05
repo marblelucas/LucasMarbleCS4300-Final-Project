b3db1aa37c949650847ab83b2260e4898b7e873a
Lucas Marble <lucasehmarble@gmail.com>
2026-05-04 22:42:55 -0400

    Update main.js

--- howtos/13_many_particles/main.js
+++ howtos/13_many_particles/main.js
@@ -11,18 +11,24 @@ const NUM_PARTICLES = 32,
 for( let i = 0; i < NUM_PARTICLES * NUM_PROPERTIES; i+= NUM_PROPERTIES ) {
   state[i] = -1 + Math.random() * 2
   state[i + 1] = -1 + Math.random() * 2
+  while(Math.abs(state[i]) < 0.5 && Math.abs(state[i + 1]) < 0.5){
+    state[i] = -1 + Math.random() * 2
+    state[i + 1] = -1 + Math.random() * 2
+  }
   state[i + 2] = -1 + Math.random() * 2
   state[i + 3] = -1 + Math.random() * 2
   state[i + 4] = 1.0 
-  state[i + 5] = 0.0 
-  state[i + 6] = 0.0 
-  state[i + 7] = 0.0 
+  state[i + 5] = Math.floor(Math.random() * 6 - 1);
+  state[i + 6] = -1 + Math.random() * 2
+  state[i + 7] = -1 + Math.random() * 2
 }
 
 state[0] = 0;
 state[1] = 0;
 state[2] = 0;
 state[3] = 0;
+state[5] = 0;
+
 
 const usage = GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST
 
@@ -31,10 +37,12 @@ const state_b = sg.buffer( state, '', usage ),
       res_u   = sg.uniform([ sg.width, sg.height ]),
       x_move  = sg.uniform( 0 ),
       y_move  = sg.uniform( 0 ),
-      mode   = sg.uniform( 0 )
+      mode    = sg.uniform( 0 ),
+      back    = new Float32Array( seagulls.width * seagulls.height * 4 ),
+      trail   = sg.texture( back )
 
 window.addEventListener('keydown', (e) => {
-  if (e.code === 'KeyD') x_move.value = 2.0;
+  if (e.code === 'KeyD') x_move.value = 4.0;
 });
 
 window.addEventListener('keyup', (e) => {
@@ -42,7 +50,7 @@ window.addEventListener('keyup', (e) => {
 });
 
 window.addEventListener('keydown', (e) => {
-  if (e.code === 'KeyA') x_move.value = -2.0;
+  if (e.code === 'KeyA') x_move.value = -4.0;
 });
 
 window.addEventListener('keyup', (e) => {
@@ -50,7 +58,7 @@ window.addEventListener('keyup', (e) => {
 });
 
 window.addEventListener('keydown', (e) => {
-  if (e.code === 'KeyW') y_move.value = 2.0;
+  if (e.code === 'KeyW') y_move.value = 4.0;
 });
 
 window.addEventListener('keyup', (e) => {
@@ -58,20 +66,20 @@ window.addEventListener('keyup', (e) => {
 });
 
 window.addEventListener('keydown', (e) => {
-  if (e.code === 'KeyS') y_move.value = -2.0;
+  if (e.code === 'KeyS') y_move.value = -4.0;
 });
 
 window.addEventListener('keyup', (e) => {
   if (e.code === 'KeyS') y_move.value = 0.0;
 });
 
-setInterval(() => {
-  mode.value = (mode.value + 1) % 4;
-  
-  document.getElementById('mode').textContent = "Straight";
+const modeSwitcher = setInterval(() => {
+  mode.value = (mode.value + 1) % 7;
+  let key = 2;
   switch (mode.value){
     case 0:
       document.getElementById('mode').textContent = 'Straight';
+      key++;
       break;
     case 1:
       document.getElementById('mode').textContent = 'Orbit';
@@ -81,6 +89,15 @@ setInterval(() => {
       break;
     case 3:
       document.getElementById('mode').textContent = 'Ricochet';
+      break;
+    case 4:
+      document.getElementById('mode').textContent = 'Arc';
+      break;
+    case 5:
+      document.getElementById('mode').textContent = 'Wind';
+      break;
+    case 6:
+      document.getElementById('mode').textContent = 'Pressure';
       break;
   }
 
@@ -103,16 +120,17 @@ function increaseTimer(){
 
 const timer = setInterval(increaseTimer, 1000);
 
-const checker = setInterval(checkPlayerAlive, 100);
-
 const render = await sg.render({
   shader: render_shader,
   data: [
     frame_u,
     res_u,
     state_b,
+    trail,
+    sg.sampler()
   ],
   onframe() { frame_u.value++ },
+  copy: trail,
   count: NUM_PARTICLES,
   blend: true
 })
@@ -133,15 +151,16 @@ const compute = sg.compute({
 
 })
 
+setInterval(checkPlayerAlive, 100);
+
 async function checkPlayerAlive() {
   
 
   const alive = await state_b.read(null, 0, Float32Array);
 
-  console.log(alive[4]);
-
   if (alive[4] === 0) {
     clearInterval(timer);
+    clearInterval(modeSwitcher);
   }
 }
 
